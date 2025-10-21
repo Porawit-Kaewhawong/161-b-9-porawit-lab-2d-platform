228e68d9ddb71e30b02e525fb4bfa90b7ffa1c20
Porawit-Kaewhawong <porawit.kaew@bumail.net>
2025-10-21 15:36:34 +0700

    Ant's Point A and B

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class Ant : Enemy
 {
-    public override void Behavoir()
-    {
-        throw new System.NotImplementedException();
-    }
+    [SerializeField] Vector2 velocity;
+    public Transform[] MovePoints;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.Initialized(20);
+    }
+
+    public override void Behavoir()
+    {
+        throw new System.NotImplementedException();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Crocodile.cs
+++ Assets/Scripts/Crocodile.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class Crocodile : Enemy
 {
-    public override void Behavoir()
-    {
-        throw new System.NotImplementedException();
-    }
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.Initialized(50);
+    }
+
+    public override void Behavoir()
+    {
+        throw new System.NotImplementedException();
     }
 
     // Update is called once per frame
